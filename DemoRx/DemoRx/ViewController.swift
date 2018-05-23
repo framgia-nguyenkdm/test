a5a478a0d272feb6e756e08c6cbe5d18fe1bf550
Khuc Dinh Minh Nguyen <khuc.dinh.minh.nguyen@framgia.com>
2018-05-23 15:57:07 +0700

    Init

---
+++ DemoRx/DemoRx/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DemoRx
+//
+//  Created by khuc.d.m.nguyen on 5/23/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
